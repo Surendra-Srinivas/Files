4c07fcc7fbd572b8ef2170c9b6b8e6738550ae32
Surendra Srinivas <83644731+Surendra-Srinivas@users.noreply.github.com>
2022-06-16 14:46:42 +0530

    Update addnums.py

--- addnums.py
+++ addnums.py
@@ -1,3 +1,12 @@
+def ultimate(power):
+    def supreme(func):
+        def inner(list):
+            return [func(val[0], val[1])**power for val in list]
+        return inner
+    return supreme
+
+@ultimate(2)
 def add(x,y):
     return x+y
-add(3,4)
+
+print(add([(1, 3), (3, 17), (5, 5), (6, 7)]))
